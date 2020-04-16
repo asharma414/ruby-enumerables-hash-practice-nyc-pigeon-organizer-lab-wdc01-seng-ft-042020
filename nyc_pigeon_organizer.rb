8004f319ac4410ef4da95d859dccb9f6f92f9f82
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-16 06:00:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,18 +16,18 @@ def nyc_pigeon_organizer(data)
     end
   end
     data[:gender].each do |key, value|
-    value.each do |person|
-      if hash.has_key?(person)
-        hash[person][:gender] << key.to_s
+      value.each do |person|
+        if hash.has_key?(person)
+          hash[person][:gender] << key.to_s
+        end
       end
     end
-  end
-      data[:lives].each do |key, value|
-    value.each do |person|
-      if hash.has_key?(person)
-        hash[person][:lives] << key.to_s
+    data[:lives].each do |key, value|
+      value.each do |person|
+        if hash.has_key?(person)
+          hash[person][:lives] << key.to_s
+        end
       end
     end
-  end
   hash
 end
