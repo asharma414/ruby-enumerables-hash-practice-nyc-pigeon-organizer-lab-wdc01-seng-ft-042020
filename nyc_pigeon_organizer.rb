e3e9c6cbf6aa81661e9d818ba7ddada824a1bff7
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-16 05:58:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data[:gender][:male].each do |ele|
+    hash[ele] = { color: [], gender: [], lives: [] }
+  end
+    data[:gender][:female].each do |ele|
+    hash[ele] = { color: [], gender: [], lives: [] }
+  end
+  data[:color].each do |key, value|
+    value.each do |person|
+      if hash.has_key?(person)
+        hash[person][:color] << key.to_s
+      end
+    end
+  end
+    data[:gender].each do |key, value|
+    value.each do |person|
+      if hash.has_key?(person)
+        hash[person][:gender] << key.to_s
+      end
+    end
+  end
+      data[:lives].each do |key, value|
+    value.each do |person|
+      if hash.has_key?(person)
+        hash[person][:lives] << key.to_s
+      end
+    end
+  end
+  hash
 end
